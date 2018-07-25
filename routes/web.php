0b78e1f4dab34617bc89f69c52f1c3e417f758ac
a.pukay <a.pukay@1plus1.digital>
2018-07-25 13:10:39 +0300

    Laravel update

--- routes/web.php
+++ routes/web.php
@@ -14,39 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/tree', 'StaffController@tree');
-
-Route::get('/list', 'StaffController@list');
-
-Route::get('/person/{id}', 'StaffController@getPerson');
-
-Route::post('/getChiefs', 'StaffController@getChiefs');
-
-Route::post('/updatePerson', 'StaffController@updatePerson');
-
-Route::post('/deletePerson', 'StaffController@deletePerson');
-
-Route::post('/cancelUpdatePerson', 'StaffController@cancelUpdatePerson');
-
-Route::post('/uploadImage', 'UploadImageController@uploadImage');
-
-Route::post('/searchBoss', 'StaffController@searchBoss');
-
-Route::post('/getIdByName', 'StaffController@getIdByName');
-
-//Route::post('/getList', 'StaffController@getListStaff');
-Route::post('/getList', 'StaffController@getListStaff');
-
-//Route::post('/searchInList', 'StaffController@searchInList');
-Route::post('/searchInList', 'StaffController@searchInList');
-//Route::get('/searchInList', 'StaffController@searchInList');
-var
-
-Route::post('/insertStaff', 'StaffController@insertStaff');
-
